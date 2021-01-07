291324a18563eb89d7639a64d25ac58beb663bb8
kojimakei <kojimaruuuuu@gmail.com>
2021-01-08 07:09:32 +0900

    簡単ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  # ゲストユーザーを削除できないようにする
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: 'stacks#index'
   resources :stacks
+
+  # 簡単ログイン機能のルーティン
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
