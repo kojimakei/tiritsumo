70d2c3a5531aa0d0b81272c4add0d16882944c29
kojimakei <kojimaruuuuu@gmail.com>
2021-01-08 07:09:19 +0900

    usersコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ApplicationRecord
     validates :nickname
     validates :password
   end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.nickname = "test"
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
