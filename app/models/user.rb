54d375f104284b3ac152cf3c241d8c994942bc89
kojimakei <kojimaruuuuu@gmail.com>
2021-01-08 07:52:00 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :stacks
+  has_many :comments
   with_options presence: true do
     validates :email
     validates :nickname
