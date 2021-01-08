cf0926d91f267d46acec309aa0a93fce1e7b6df4
kojimakei <kojimaruuuuu@gmail.com>
2021-01-08 16:06:56 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  
+
+
+  root to: 'stacks#index'
+  resources :stacks do
+    resources :comments, only: [:create, :destroy]
+  end
+
   # ゲストユーザーを削除できないようにする
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  root to: 'stacks#index'
-  resources :stacks
 
   # 簡単ログイン機能のルーティン
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+
 end
