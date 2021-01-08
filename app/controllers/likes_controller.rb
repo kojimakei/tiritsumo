efc210efc816e76813f1c40f2749082d77df41b4
kojimakei <kojimaruuuuu@gmail.com>
2021-01-09 07:51:15 +0900

    ビューファイルの設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,19 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
 
   def create
-    @like = current_user.likes
-    @like.create(stack_id: params[:stack_id])
-    redirect_to root_path
+    @like = current_user.likes.new(stack_id: params[:stack_id])
+    if @like.save
+    redirect_to stack_path(@like.stack)
+    else
+      render 'stacks/show'
+    end
   end
 
   def destroy
     @like = Like.find_by(stack_id: params[:stack_id], user_id: current_user.id)
     @like.destroy
-    redirect_to root_path
+    redirect_to stack_path(@like.stack)
   end
 end
 
