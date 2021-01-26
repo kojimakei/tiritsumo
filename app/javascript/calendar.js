a69dc4074c43615e114c257b17675dd1f95c8b18
kojimakei <kojimaruuuuu@gmail.com>
2021-01-27 07:38:29 +0900

    カレンダー機能のイベントを仮で追加

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -1,39 +1,56 @@
-// ※プラグイン※
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 
+document.addEventListener('turbolinks:load', function() {
+  var calendarEl = document.getElementById('calendar');
 
-
-  function initCalendar(){
-    var calendarEl = document.getElementById('calendar');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ Calendar, dayGridPlugin, interactionPlugin, timeGridPlugin ],
-      initialView: 'timeGridWeek',
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin ],
+    initialView: 'timeGridWeek',
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
 
-      // ※日本語表記に変更※
-      locale: 'ja',
-      height: 'auto',
-      firstDay: 1,
-      buttonText: {
+    events: [
+      {
+          id: '1',
+          title: '筋トレ',
+          start: '2021-01-24T10:00:00',
+          end: '2021-01-24T12:00:00',
+          url: '#'
+      },
+      {
+          id: '2',
+          title: '先輩とご飯',
+          start: '2021-01-23T10:00:00',
+          url: '#'
+      },
+      {
+          id: '3',
+          title: 'ポートフォリオ作成',
+          start: '2021-01-18T15:00:00',
+          end: '2021-01-18T19:00:00', 
+          url: '#'
+      },
+        
+  ],
+
+    // ※日本語表記に変更※
+    locale: 'ja',
+    height: 'auto',
+    firstDay: 1,
+    buttonText: {
       today: '今日',
       month: '月',
       list: 'リスト',     
-      },       
-      
-      events: '/events.json',
-    });
-  
-    calendar.render();
-  }
+    }, 
 
-  function addEvent(event){
-    calendar.addEvent(event);
-  }
+  });
+
+  calendar.render();
+
+});
