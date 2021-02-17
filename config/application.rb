0fc6f9661f3cbb8b1a7e209343d3686ab7ea7b25
kojimakei <kojimaruuuuu@gmail.com>
2021-02-17 16:35:04 +0900

    データベースのタイムゾーン設定をUTC変更

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,10 @@ module Tiritsumo
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
+    Groupdate.time_zone = "Tokyo"
+    # ↓デフォルトで記載あったが削除。データベースのタイムゾーン↓
+    # config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
