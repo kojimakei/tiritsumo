c80f2ffccebfb5e6c879abd70b477fbaa1940d6f
kojimakei <kojimaruuuuu@gmail.com>
2021-02-26 17:12:36 +0900

    各roomのviewを調整

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,10 @@
 class RoomsController < ApplicationController
+  before_action :set_stacks, only: [:show, :edit, :update, :destroy, :join]
+  before_action :owner_user, only: [:edit, :update, :delete, :destroy]
 
   def index
     @rooms = Room.all
+    @user_rooms = UserRoom.all
   end
 
   def new
@@ -20,14 +23,27 @@ class RoomsController < ApplicationController
   end
 
   def show
-    @room = Room.find(params[:id])
+    @user_rooms = UserRoom.all
     @chats = @room.chats.includes(:user)
     @chat = current_user.chats.new
+  end
+
+  def join
+    @room.users << current_user
+    redirect_to rooms_path
   end
 
   private
   def room_params
     params.require(:room).permit(:name)
   end
-end
 
+  def set_stacks
+    @room = Room.find(params[:id])
+  end
+
+  def owner_user
+    @group = Room.find(params[:id])
+    redirect_to rooms_path unless @room.owner?(current_user) || current_user.admin?
+  end
+end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
-  has_many :chats
-  has_many :user_rooms
+  has_many :chats, dependent: :destroy
+  has_many :user_rooms, dependent: :destroy
   has_many :users, through: :user_rooms
   validates :name, presence: true
 end
