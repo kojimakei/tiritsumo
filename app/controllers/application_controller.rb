943324ff4f694a2091c1e5793d97e644f503a3d0
kojimakei <kojimaruuuuu@gmail.com>
2021-03-09 12:15:54 +0900

    アクションごとにflashを表示する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :goal, :deadline, :age_id, :category_id, :occupation_id])
   end
 
-  # renderer.renderの代替メソッドを用意
+  # render.renderの代替メソッドを用意
   def self.render_with_signed_in_user(user, *args)
     ActionController::Renderer::RACK_KEY_TRANSLATION['warden'] ||= 'warden'
     proxy = Warden::Proxy.new({}, Warden::Manager.new({})).tap { |i| i.set_user(user, scope: :user) }
