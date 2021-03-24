0c760e3c86a9a828c124f7d6febb3927df31eccd
kojimakei <kojimaruuuuu@gmail.com>
2021-03-24 18:53:59 +0900

    headにpack_tag追加

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Tiritsumo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]    
     # Groupdate.time_zone = "Tokyo"
     # ↓デフォルトで記載あったが削除。データベースのタイムゾーン↓
     # config.active_record.default_timezone = :local
