aa8fc690a133b577529f6c9b7d8876ffca98c7ed
kojimakei <kojimaruuuuu@gmail.com>
2021-03-24 17:23:06 +0900

    timezone変更

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module Tiritsumo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
-    config.time_zone = 'Asia/Tokyo'
-    config.beginning_of_week = :sunday
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]    
     # Groupdate.time_zone = "Tokyo"
     # ↓デフォルトで記載あったが削除。データベースのタイムゾーン↓
     # config.active_record.default_timezone = :local
