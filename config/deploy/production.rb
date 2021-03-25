6ac2049839643104db8beb64c11111414e58fe5f
kojimakei <kojimaruuuuu@gmail.com>
2021-03-25 17:35:41 +0900

    sshを環境変数ではなく、ファイルで指定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,4 @@
 server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
 # CircleCIのGUIで設定した環境変数を使ってSSH接続
-set :ssh_options, {
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
-  forward_agent: true,
-  auth_methods: %w[publickey]
-}
+set :ssh_options, keys: '~/.ssh/id_rsa'
