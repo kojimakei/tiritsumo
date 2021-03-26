4fa1a3135a9752b2b2d1c49463672d8d8a739160
kojimakei <kojimaruuuuu@gmail.com>
2021-03-26 14:06:32 +0900

    ec2インスタンスへの鍵に変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
 
 set :ssh_options, auth_methods: ['publickey'],
-                                   keys: ['~/.ssh/tiritsumo.pem'] 
+                                   keys: ['~/.ssh/id_rsa'] 
