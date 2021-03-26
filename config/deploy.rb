38f4aa6ae7d0c0254302be8f46cae2ea130854ea
kojimakei <kojimaruuuuu@gmail.com>
2021-03-26 09:17:27 +0900

    deploy.rbを編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,15 @@ set :application, 'tiritsumo'
 
 set :repo_url,  'git@github.com:kojimakei/tiritsumo.git'
 
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/tiritsumo"
+
+# secret_base_keyを読み込ませるため追記
+set :linked_files, %w[config/master.key]
+
+# シンボリックリンクをはるファイル。
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/settings.yml', '.env')
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
