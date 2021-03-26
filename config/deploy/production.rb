64865e4353e001c4edd8396e17175d4166f97934
kojimakei <kojimaruuuuu@gmail.com>
2021-03-26 09:32:27 +0900

    production.rbを編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,8 @@
 server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
 # CircleCIのGUIで設定した環境変数を使ってSSH接続
-set :ssh_options, keys: '~/.ssh/id_rsa.pub'
+set :ssh_options,s, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
