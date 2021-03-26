b85d2ec32bf9cf0806bdad95b9ca5c926ba238c0
kojimakei <kojimaruuuuu@gmail.com>
2021-03-26 14:03:27 +0900

    ssh_options追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
-# CircleCIのGUIで設定した環境変数を使ってSSH接続
 
+set :ssh_options, auth_methods: ['publickey'],
+                                   keys: ['~/.ssh/tiritsumo.pem'] 
