60d438e0d1a4110f888f5f6058964c6e487cdd74
kojimakei <kojimaruuuuu@gmail.com>
2021-03-26 12:46:38 +0900

    ssh_optionsを再編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,7 @@ server '52.199.151.77', user: 'ec2-user', roles: %w{app db web},
 ssh_options: {
   port: 22,
   user: "ec2-user", # overrides user setting above
-  keys: %w(~/.ssh/tiritsumo.pem),
-  forward_agent: true
+  keys: [File.expand_path('~/.ssh/id_rsa')],#'~/.ssh/id_rsa'部分をローカル環境の鍵のパスに
+  forward_agent: true,
+  auth_methods: %w(publickey)
 }
