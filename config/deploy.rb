5528501554143fe72ef6f40948892c30c5f118cf
kojimakei <kojimaruuuuu@gmail.com>
2021-03-28 22:43:42 +0900

    ssh_optionsを削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,8 +18,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/tiritsumo.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
