bab03842268dc86994362f75e25605e618948704
kojimakei <kojimaruuuuu@gmail.com>
2021-03-28 10:02:10 +0900

    circleciに表示された任意の文字列をsshkeyに指定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
 
 set :ssh_options, auth_methods: ['publickey'],
-                                   keys: ['7c:42:54:83:59:4b:4d:76:18:a4:3f:4a:63:8a:11:62'] 
+                                   keys: ['~/.ssh/id_rsa_ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw=='] 
