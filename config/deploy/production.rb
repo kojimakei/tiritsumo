f2e46ca9301d7f1f4daa2bb66045a588cdf89bba
kojimakei <kojimaruuuuu@gmail.com>
2021-03-28 11:05:53 +0900

    keysを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
 
 set :ssh_options, auth_methods: ['publickey'],
-                                   keys: ['~/.ssh/id_rsa_7c:42:54:83:59:4b:4d:76:18:a4:3f:4a:63:8a:11:62'] 
+                                   keys: ['~/.ssh/id_rsa_1c199388c59ec02fd4601ec64d4e9644'] 
