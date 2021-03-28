40e40507846a0f86c778d7d6608cf46d7e6e8dc9
kojimakei <kojimaruuuuu@gmail.com>
2021-03-28 13:28:25 +0900

    deploy.rbを編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,13 @@ set :linked_files, %w[config/master.key]
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  auth_methods: %w[publickey]
+}
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
