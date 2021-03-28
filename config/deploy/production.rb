ae9078a95c78c9d98779ebcb2ac12f1c2445fa23
kojimakei <kojimaruuuuu@gmail.com>
2021-03-28 11:53:11 +0900

    CircleCIのGUIで設定した環境変数を使ってSSH接続

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,9 @@
 server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
 
-set :ssh_options, auth_methods: ['publickey'],
-                                   keys: ['~/.ssh/id_rsa_1c199388c59ec02fd4601ec64d4e9644'] 
+# CircleCIのGUIで設定した環境変数を使ってSSH接続
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
