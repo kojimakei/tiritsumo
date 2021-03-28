e2e3182b9aeae686fb184001391026540c97b69b
kojimakei <kojimaruuuuu@gmail.com>
2021-03-29 08:49:33 +0900

    fingerprintsを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
 
 # CircleCIのGUIで設定した環境変数を使ってSSH接続
-set :ssh_options, auth_methods: ['publickey'],
-                                   keys: ['~/.ssh/id_rsa_7c425483594b4d7618a43f4a638a1162'] 
+# set :ssh_options, auth_methods: ['publickey'],
+#                                    keys: ['~/.ssh/id_rsa_1c199388c59ec02fd4601ec64d4e9644'] 
