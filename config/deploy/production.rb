86d4a607c16a261e0d9eca38c71226c4e9b9da3b
kojimakei <kojimaruuuuu@gmail.com>
2021-03-28 10:59:54 +0900

    :ありに変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server '52.199.151.77', user: 'ec2-user', roles: %w{app db web}
 
 
 set :ssh_options, auth_methods: ['publickey'],
-                                   keys: ['~/.ssh/id_rsa_7c425483594b4d7618a43f4a638a1162 '] 
+                                   keys: ['~/.ssh/id_rsa_7c:42:54:83:59:4b:4d:76:18:a4:3f:4a:63:8a:11:62'] 
