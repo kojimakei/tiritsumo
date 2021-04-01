44013b91f6452369af0f8bfd522e4dc319671801
kojimakei <kojimaruuuuu@gmail.com>
2021-04-01 09:12:11 +0900

    socket: /var/lib/mysql/mysql.sockを削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # Herokuを用いてデプロイする場合にのみ必要な設定
+  # awsを用いてデプロイする場合にのみ必要な設定
   ActionCable.server.config.disable_request_forgery_protection = true
   config.action_cable.url = 'ws://52.199.151.77/cable'
   config.action_cable.allowed_request_origins = [ 'http://52.199.151.77' ]  
