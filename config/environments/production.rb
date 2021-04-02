3a228ee669d77c4f284f9638b4bed839dbf82918
kojimakei <kojimaruuuuu@gmail.com>
2021-04-02 09:43:44 +0900

    本番環境で一部css反映されないためconfig.assets.debug = false記載

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  
+  # ↓本番環境で一部css反映されないため記載
+  config.assets.debug = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
