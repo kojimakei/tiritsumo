db1297e9080decb3b0cfb360d07a85dda6b07112
kojimakei <kojimaruuuuu@gmail.com>
2021-04-04 16:31:11 +0900

    ログインした状態でないと、ユーザー詳細ページに移動できない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update, :show]
   before_action :set_user, only: [:edit, :update, :show,]
 
   def index
